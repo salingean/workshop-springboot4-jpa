072f08f1e444cea56ae956099e516470bc740fd4
Salin Gean <salingeanestrela@gmail.com>
2026-05-19 20:40:43 -0300

    Product

---
+++ src/main/java/com/salingean/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.salingean.course.repositories;
+
+import com.salingean.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
