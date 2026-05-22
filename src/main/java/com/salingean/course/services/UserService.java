2dee8a0591371308f73fa07c7282fef03fcc523d
Salin Gean <salingeanestrela@gmail.com>
2026-05-22 16:33:15 -0300

    Exception handling - findById

--- src/main/java/com/salingean/course/services/UserService.java
+++ src/main/java/com/salingean/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.salingean.course.services;
 
 import com.salingean.course.entities.User;
 import com.salingean.course.repositories.UserRepository;
+import com.salingean.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
