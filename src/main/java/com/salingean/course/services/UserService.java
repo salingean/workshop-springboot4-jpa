5c35871da045941d16fba5514fb18fa5697c468a
Salin Gean <salingeanestrela@gmail.com>
2026-05-22 17:12:12 -0300

    Exception handling - delete

--- src/main/java/com/salingean/course/services/UserService.java
+++ src/main/java/com/salingean/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.salingean.course.services;
 
 import com.salingean.course.entities.User;
 import com.salingean.course.repositories.UserRepository;
+import com.salingean.course.services.exceptions.DatabaseException;
 import com.salingean.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,11 +28,17 @@ public class UserService {
     }
 
     public User insert(User obj) {
-        return repository.save(obj);
+            return repository.save(obj);
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
