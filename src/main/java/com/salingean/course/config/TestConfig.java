50cc99150c0730182ac82ab559b2b5c67eb2f22d
Salin Gean <salingeanestrela@gmail.com>
2026-05-22 14:57:32 -0300

    Payment, one-to-one association

--- src/main/java/com/salingean/course/config/TestConfig.java
+++ src/main/java/com/salingean/course/config/TestConfig.java
@@ -60,7 +60,7 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING, u2);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
@@ -72,5 +72,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay2 = new Payment(null, Instant.parse("2019-07-20T21:42:10Z"), o2);
+        o2.setPayment(pay2);
+
+        orderRepository.save(o2);
     }
 }
